56bc163d5ee01bbe3abce1fe78870fd9c85b1c83
Mohamad Murad <30772068+mohamadmurad@users.noreply.github.com>
2019-05-24 02:27:01 +0300

    url

--- app/src/main/java/com/example/project/AppConfig.java
+++ app/src/main/java/com/example/project/AppConfig.java
@@ -2,6 +2,6 @@ package com.example.project;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.8/autism-web/api/login.php";
+    public static String URL_LOGIN = "http://syad3.000webhostapp.com/autism-web/api/login.php";
 
 }
